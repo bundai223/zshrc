909140138a031ccaaacaee698e9c9dcac7fddf41
Daiji Nishimura <daiji@DaijinoMacBook-Pro.local>
2016-12-11 20:25:55 +0900

    add scripts.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 conf_dir=~/
 dir=$(cd $(dirname $0)/../ && pwd)
 ln -s $dir/.zshrc $conf_dir/.zshrc
 ln -s $dir/.zshenv $conf_dir/.zshenv
+
+if [ "$(uname)" == 'Darwin' ]; then
+  ln -s $dir/.zshrc_osx $conf_dir/.zshrc_local
+  ln -s $dir/.zshenv_osx $conf_dir/.zshenv_local
+
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  OS='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+  OS='Cygwin'
+else
+  echo "Your platform ($(uname -a)) is not supported."
+fi
